6ab67689b3304b99b7a4faaf3b30c91725c82545
feebee <pabbruzz@purdue.edu>
2025-04-12 21:31:39 -0400

    nav bar

--- website/my-app/src/home.js
+++ website/my-app/src/home.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from "react";
-
+import NavBar from "./NavBar";
 
 
 function Home() {
   return (
-    <p className="title">Welcome Home!</p>
+    <div>
+      <NavBar />
+      <p className="title">Home page</p>
+    </div>
   );
 }
 
--- website/my-app/src/index.js
+++ website/my-app/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Home from './home';
+
 import reportWebVitals from './reportWebVitals';
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
